4eb473a5050ab33b21a7d9bb1d27eb5fce096c79
Manuel Rivera <menu.rivera@gmail.com>
2023-11-24 16:27:11 -0600

    [ADD] -> ARWEAVE_WALLET env variable and dummy contract template

--- api/_lib/createContract.ts
+++ api/_lib/createContract.ts
@@ -5,7 +5,28 @@ interface IBody {
 }
 
 export const createContract = ({ org, name, contractId }: IBody): string => {
-	// extract body information	
-	
-	return ''
+	console.log('Creating contract...')
+	const currentDate = new Date()
+	const formattedDate = currentDate.toDateString()
+
+	return `
+  Ownership Contract
+
+  This contract is entered into on ${formattedDate} by and between ${org} (hereinafter referred to as the "Organization") and ${name} (hereinafter referred to as the "Owner").
+
+  1. Object of the Contract
+  The Organization agrees to grant ownership rights to the Owner for the specified contract identified by the Contract ID: ${contractId}.
+
+  2. Terms and Conditions
+  - The ownership rights include, but are not limited to, usage, transfer, and any other privileges associated with the specified contract.
+  - The Owner agrees to comply with all applicable laws and regulations related to the ownership of the specified contract.
+
+  3. Contract ID
+  The Contract ID for this ownership agreement is ${contractId}.
+
+  4. Governing Law
+  This ownership contract shall be governed by and construed in accordance with the laws of [Jurisdiction].
+
+  IN WITNESS WHEREOF, the parties hereto have executed this ownership contract as of the date first above written.
+	`
 }
--- api/_lib/uplaodToArweave.ts
+++ api/_lib/uplaodToArweave.ts
@@ -2,7 +2,10 @@ import Irys from "@irys/sdk"
 import fs from 'fs'
 
 export const uploadToArweave = async (doc: string) => {
-	const key = JSON.parse(fs.readFileSync('wallet.json').toString())
+	console.log('Uploading to arweave...')
+	// const key = JSON.parse(fs.readFileSync('wallet.json').toString())
+	const key = process.env.ARWEAVE_WALLET
+	console.log({ key })
 
 	const irys = new Irys({
 		url: 'https://node2.irys.xyz',
