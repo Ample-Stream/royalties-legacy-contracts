4aba17de8bd8d4cbcb0bbd75db5db216e432fc63
Manuel Rivera <menu.rivera@gmail.com>
2023-11-24 23:29:44 -0600

    [REPLACE]  -> cors from vercel.json to middleware-like wrapper

--- api/contract.ts
+++ api/contract.ts
@@ -2,7 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import { createContract } from './_lib/createContract';
 import { uploadToArweave } from './_lib/uplaodToArweave';
 
-export default async (req: VercelRequest, res: VercelResponse) => {
+const handler = async (req: VercelRequest, res: VercelResponse) => {
 	if (req.method != 'POST') return res.status(500).send('');
 
 	try {
@@ -32,3 +32,19 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
 	}
 }
+
+const allowCors = (fn) => async (req, res) => {
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+	res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+	if (req.method === 'OPTIONS') {
+		res.status(200).end();
+		return;
+	}
+
+	return await fn(req, res);
+}
+
+export default allowCors(handler)
