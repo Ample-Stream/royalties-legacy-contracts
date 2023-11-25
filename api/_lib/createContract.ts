3627fb93673ed18fba77f8519cb904a3df4316b1
Manuel Rivera <menu.rivera@gmail.com>
2023-11-24 18:34:52 -0600

    [MOD|FIX] -> plain text pdf creation

--- api/_lib/createContract.ts
+++ api/_lib/createContract.ts
@@ -12,32 +12,29 @@ export const createContract = ({ org, name, contractId }: IBody): Buffer => {
 	const currentDate = new Date()
 	const formattedDate = currentDate.toDateString()
 
-	const htmlDoc = `
-	  <h2>Ownership Contract</h2>
+	const stringDoc = `
+	  Ownership Contract
 
-	  <p>This contract is entered into on ${formattedDate} by and between ${org} (hereinafter referred to as the "Organization") and ${name} (hereinafter referred to as the "Owner").</p>
+  This contract is entered into on ${formattedDate} by and between ${org} (hereinafter referred to as the "Organization") and ${name} (hereinafter referred to as the "Owner").
 
-	  <h3>1. Object of the Contract</h3>
-	  <p>The Organization agrees to grant ownership rights to the Owner for the specified contract identified by the Contract ID: ${contractId}.</p>
+  1. Object of the Contract
+  The Organization agrees to grant ownership rights to the Owner for the specified contract identified by the Contract ID: ${contractId}.
 
-	  <h3>2. Terms and Conditions</h3>
-	  <ul>
-		<li>The ownership rights include, but are not limited to, usage, transfer, and any other privileges associated with the specified contract.</li>
-		<li>The Owner agrees to comply with all applicable laws and regulations related to the ownership of the specified contract.</li>
-	  </ul>
+  2. Terms and Conditions
+  - The ownership rights include, but are not limited to, usage, transfer, and any other privileges associated with the specified contract.
+  - The Owner agrees to comply with all applicable laws and regulations related to the ownership of the specified contract.
 
-	  <h3>3. Contract ID</h3>
-	  <p>The Contract ID for this ownership agreement is ${contractId}.</p>
+  3. Contract ID
+  The Contract ID for this ownership agreement is ${contractId}.
 
-	  <h3>4. Governing Law</h3>
-	  <p>This ownership contract shall be governed by and construed in accordance with the laws of [Jurisdiction].</p>
+  4. Governing Law
+  This ownership contract shall be governed by and construed in accordance with the laws of [Jurisdiction].
 
-	  <p><em>IN WITNESS WHEREOF, the parties hereto have executed this ownership contract as of the date first above written.</em></p>
+  IN WITNESS WHEREOF, the parties hereto have executed this ownership contract as of the date first above written.
 	`
 
 	const doc = new jsPDF()
-
-	doc.html(htmlDoc, { margin: 2 })
+	doc.text(stringDoc, 5, 5)
 
 	return Buffer.from(doc.output())
 }
